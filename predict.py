2a697dc23908d21d704ea9c7863f2eacfc5f1a86
Tomasz Slowikowski <tslowikowski@vp.pl>
2019-03-19 16:24:35 +0100

    tuning

--- predict.py
+++ predict.py
@@ -4,46 +4,47 @@ import numpy as np
 import fastText
 
 
-def preprocess_texts(raw_texts,replacements):
+def preprocess_texts(raw_texts, replacements):
     """
     texts: np.Series containing strings to be preprocessed
     replacements: pairs of what convert to what
     return np.Series with corrected texts
     """
-    resulttext=raw_texts.str.lower()
-    for [co,naco,_] in replacements.values:
-        resulttext=resulttext.str.replace(re.compile(str(co)),str(naco))
+    resulttext = raw_texts.str.lower()
+    for [co, naco, _] in replacements.values:
+        resulttext = resulttext.str.replace(re.compile(str(co)), str(naco))
     return resulttext
 
 
 # interesting_labels=pd.read_csv(os.path.join(datapath,'interesting_labels.csv'))
 
 def make_formatted_predictions(clf, texts):
-    predictions=[]
+    predictions = []
     for t in texts:
-        labels, probs = clf.predict(t,k=5,threshold=0.1)
-        result_line=''
+        labels, probs = clf.predict(t, k=5, threshold=0.1)
+        result_line = ''
         # print(f'labels:{labels}')
         # print(f'probs:{probs}')
-        for i,l in enumerate(labels):
+        for i, l in enumerate(labels):
             # print(f'i:{i} l:{l} probs[i]:{probs[i]}')
-            l=l.replace('__label__','')
-            result_line+=l + ': ' + str(probs[i]) + '; '
+            l = l.replace('__label__', '')
+            result_line += l + ': ' + str(probs[i]) + '; '
         # print(f'result_line: {result_line}')
         predictions.append(result_line)
     return predictions
 
-datapath='.'
+
+datapath = '.'
 print('loading cases for prediction...')
-texts_test=pd.read_csv(os.path.join(datapath,'cases.txt'), header=None)
+texts_test = pd.read_csv(os.path.join(datapath, 'cases.txt'), header=None)
 print('loading preprocessing configuration...')
-podmiany=pd.read_excel(os.path.join(datapath,'preproc_dict.xlsx'))
+podmiany = pd.read_excel(os.path.join(datapath, 'preproc_dict.xlsx'))
 print('preprocessing...')
-test_set=preprocess_texts(texts_test[0],podmiany)
+test_set = preprocess_texts(texts_test[0], podmiany)
 print('loading model...')
-classifier=fastText.load_model(os.path.join(datapath,'model'))
+classifier = fastText.load_model(os.path.join(datapath, 'model'))
 print('predicting labels...')
 result = make_formatted_predictions(classifier, test_set.values)
 print('saving model...')
-pd.Series(result).to_csv(os.path.join(datapath,'predictions.csv'),index=False)
+pd.Series(result).to_csv(os.path.join(datapath, 'predictions.csv'), index=False)
 print('Done! Please find results in predictions.csv')
